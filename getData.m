b6a761611494e7cb5dd79e985572377284e10d7d
fjzzlkx <fjzzlkx@163.com>
2016-07-28 16:34:44 +0800

    Create getData.m

---
+++ getData.m
@@ -0,0 +1,46 @@
+%直接无脑方式将data里面的cell转换成数组
+%资源池数_cellularlink数_D2Dlink数量
+a_50_10_100 = cell2mat(data(1,1))
+a_50_20_100 = cell2mat(data(1,2))
+a_50_30_100 = cell2mat(data(1,3))
+a_50_40_100 = cell2mat(data(1,4))
+
+a_50_10_300 = cell2mat(data(1,5))
+a_50_20_300 = cell2mat(data(1,6))
+a_50_30_300 = cell2mat(data(1,7))
+a_50_40_300 = cell2mat(data(1,8))
+
+a_50_10_500 = cell2mat(data(1,9))
+a_50_20_500 = cell2mat(data(1,10))
+a_50_30_500 = cell2mat(data(1,11))
+a_50_40_500 = cell2mat(data(1,12))
+
+%%%%-------------------------%%%%%
+a_100_20_100 = cell2mat(data(2,1))
+a_100_40_100 = cell2mat(data(2,2))
+a_100_60_100 = cell2mat(data(2,3))
+a_100_80_100 = cell2mat(data(2,4))
+
+a_100_20_300 = cell2mat(data(2,5))
+a_100_40_300 = cell2mat(data(2,6))
+a_100_60_300 = cell2mat(data(2,7))
+a_100_80_300 = cell2mat(data(2,8))
+
+a_100_20_500 = cell2mat(data(2,9))
+a_100_40_500 = cell2mat(data(2,10))
+a_100_60_500 = cell2mat(data(2,11))
+a_100_80_500 = cell2mat(data(2,12))
+
+%%%%-------------------------%%%%%
+a_150_30_100 = cell2mat(data(3,1))
+a_150_60_100 = cell2mat(data(3,2))
+a_150_90_100 = cell2mat(data(3,3))
+a_150_120_100 = cell2mat(data(3,4))
+a_150_30_300 = cell2mat(data(3,5))
+a_150_60_300 = cell2mat(data(3,6))
+a_150_90_300 = cell2mat(data(3,7))
+a_150_120_300 = cell2mat(data(3,8))
+a_150_30_500 = cell2mat(data(3,9))
+a_150_60_500 = cell2mat(data(3,10))
+a_150_90_500 = cell2mat(data(3,11))
+a_150_120_500 = cell2mat(data(3,12))
